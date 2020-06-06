2e92a319a7e0cf8df963d4d11dde3c0705d87f12
chandan-reddy-k <chandan@commutatus.com>
2020-06-06 17:45:49 +0530

    feat(hooks): add redux example with hooks

--- src/ReduxExample.js
+++ src/ReduxExample.js
@@ -1,34 +1,57 @@
+// METHOD:1 Functional component using react hooks
+
 import React, { Component } from 'react'
 import MainComponent from './MainComponent'
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { increment } from './redux/actions'
 
-export class ReduxExample extends Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        let { count, increment } = this.props
-        return (
-            <div>
-                <MainComponent count={count} handleFireClick={increment} example='Redux'/>
-            </div>
-        )
-    }
+function ReduxExample() {
+    const count = useSelector(state => state.count);
+    const dispatch = useDispatch()
+    return (
+        <div>
+            <MainComponent count={count} handleFireClick={() => dispatch(increment())} example='Redux' />
+        </div>
+    );
 }
 
-const mapStateToProps = state => {
-    return { count: state.count };
-};
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        increment: () => dispatch(increment()),
-    };
-};
+export default ReduxExample
 
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(ReduxExample);
+
+// METHOD:2 Class component using HOC
+
+// import React, { Component } from 'react'
+// import MainComponent from './MainComponent'
+// import { connect } from 'react-redux';
+// import { increment } from './redux/actions'
+
+// export class ReduxExample extends Component {
+//     constructor(props) {
+//         super(props)
+//     }
+//     render() {
+//         let { count, increment } = this.props
+//         return (
+//             <div>
+//                 <MainComponent count={count} handleFireClick={increment} example='Redux'/>
+//             </div>
+//         )
+//     }
+// }
+
+// const mapStateToProps = state => {
+//     return { count: state.count };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         increment: () => dispatch(increment()),
+//     };
+// };
+
+
+// export default connect(
+//     mapStateToProps,
+//     mapDispatchToProps
+// )(ReduxExample);
